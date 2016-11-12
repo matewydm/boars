992ca2a87c969e87f9bdca8c8d6577f3febfa52d
Mati Wydymanski <mucha_303@outlook.com>
2016-11-12 21:17:13 +0100

    dare2

---
+++ src/main/src/pl/edu/agh/miss/main/App.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.miss.main;
+
+/**
+ * Created by mucha on 12.11.16.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("dare");
+    }
+}
