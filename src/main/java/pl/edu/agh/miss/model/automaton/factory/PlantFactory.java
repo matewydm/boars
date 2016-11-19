b2742ff65887bce87f27fb8c96a7f4d41c12e23a
Mati Wydymanski <mucha_303@outlook.com>
2016-11-19 22:02:34 +0100

    poprawka abstrakcyjnej klasy

--- src/main/java/pl/edu/agh/miss/model/automaton/factory/PlantFactory.java
+++ src/main/java/pl/edu/agh/miss/model/automaton/factory/PlantFactory.java
@@ -1,7 +1,18 @@
 package pl.edu.agh.miss.model.automaton.factory;
 
+import pl.edu.agh.miss.model.automaton.life.Animal;
+import pl.edu.agh.miss.model.automaton.life.Gender;
+import pl.edu.agh.miss.model.automaton.life.Plant;
+
 /**
  * Created by mucha on 19.11.16.
  */
-public class PlantFactory {
+public abstract class PlantFactory {
+
+    public Plant addPlant() {
+        return plantFactoryMethod();
+    }
+
+    protected abstract Plant plantFactoryMethod();
+
 }
--- src/main/java/pl/edu/agh/miss/model/automaton/factory/PreyFactory.java
+++ src/main/java/pl/edu/agh/miss/model/automaton/factory/PreyFactory.java
@@ -1,8 +1,17 @@
 package pl.edu.agh.miss.model.automaton.factory;
 
+import pl.edu.agh.miss.model.automaton.life.Animal;
+import pl.edu.agh.miss.model.automaton.life.Gender;
+import pl.edu.agh.miss.model.automaton.life.Prey;
+
 /**
  * Created by mucha on 19.11.16.
  */
-public class PreyFactory {
+public class PreyFactory extends AnimalFactory{
+
+    @Override
+    protected Animal animalFactoryMethod(Gender gender) {
+        return new Prey(gender);
+    }
 
 }
